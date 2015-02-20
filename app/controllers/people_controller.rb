92d7a74f7a6e8cba7ff15364900ad99ce423f234
dcuddhy <dcuddhy@gmail.com>
2015-02-20 14:18:11 -0700

    Users must add title or first name

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -12,13 +12,28 @@ class PeopleController < ApplicationController
     @person = Person.find(params[:id])
   end
 
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    @person = Person.find(params[:id])
+    if @person.update(params.require(:person).permit(:first_name, :last_name, :title))
+     redirect_to people_path
+   else
+     render :edit
+   end
+  end
+
   def create
     @person = Person.new(params.require(:person).permit(:first_name, :last_name, :title))
     if @person.save
       redirect_to root_path, notice: "Person was created!"
     else
-      render :new
+      render :edit
     end
   end
+
+
 
 end
--- spec/features/person_spec.rb
+++ spec/features/person_spec.rb
@@ -20,4 +20,58 @@ feature 'Person - ' do
     end
   end
 
+  scenario 'Users can edit people' do
+    create_user email: "user@example.com"
+    person = create_person
+
+    visit root_path
+    click_on "Login"
+
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    click_on "Login"
+
+    click_on person.full_name
+    within 'h1' do
+      expect(page).to have_content(person.full_name)
+    end
+
+    click_on 'Edit'
+    fill_in 'Title', :with => 'Dr.'
+    fill_in 'First name', :with => 'Funken'
+    fill_in 'Last name', :with => 'Stein'
+    click_on 'Update'
+
+    within '.table' do
+      expect(page).to have_content('Dr. Funken Stein')
+    end
+  end
+
+  scenario 'Users must add title or first name' do
+    create_user email: "user@example.com"
+    person = create_person
+
+    visit root_path
+    click_on "Login"
+
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    click_on "Login"
+
+    click_on person.full_name
+    within 'h1' do
+      expect(page).to have_content(person.full_name)
+    end
+
+    click_on 'Edit'
+    fill_in 'Title', :with => ''
+    fill_in 'First name', :with => ''
+    fill_in 'Last name', :with => 'Stein'
+    click_on 'Update'
+
+    expect(page).to have_content('Title or First Name must be entered')
+  end
+
+
+
 end
